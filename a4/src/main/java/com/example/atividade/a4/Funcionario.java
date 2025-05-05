55f8c6b6ca501735c53cbe014b3368a2e02f108c
lcostaju <luciocj_eng@outlook.com>
2025-05-05 20:34:10 -0300

    Implement Funcionario validation and update FuncionarioTerceirizado structure; add tests for invalid hour inputs

--- a4/src/main/java/com/example/atividade/a4/Funcionario.java
+++ a4/src/main/java/com/example/atividade/a4/Funcionario.java
@@ -4,8 +4,25 @@ public class Funcionario {
     private String nome;
     private Integer horasTrabalhadas;
     private Double valorHora;
+
+    public Funcionario() {
+        //TODO Auto-generated constructor stub
+    }
+
+    public Funcionario(Integer horasTrabalhadas) {
+        this.horasTrabalhadas = validaHora(horasTrabalhadas);
+    }
+    
     public Double calcularPagamento() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'calcularPagamento'");
+    }
+
+    public Integer validaHora(Integer horasTrabalhadas){
+        if (horasTrabalhadas >= 20 && horasTrabalhadas <= 40) {
+            return horasTrabalhadas;
+        }else{
+            throw new IllegalArgumentException("As horas trabalhadas devem ser entre 20 e 40h");
+        }
     }
 }
--- a4/src/test/java/com/example/atividade/a4/FuncionarioTests.java
+++ a4/src/test/java/com/example/atividade/a4/FuncionarioTests.java
@@ -1,18 +1,52 @@
 package com.example.atividade.a4;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class FuncionarioTests {
     private Funcionario funcionario;
 
     @BeforeEach
-    public void instaciarClasseFuncionario(){
+    public void instaciarClasseFuncionario() {
         funcionario = new Funcionario();
     }
 
     @Test
-    public void testarCalculoHora(){
+    public void testarCalculoHora() {
         Double pagamento = funcionario.calcularPagamento();
+    }
+
+    @Test
+    @DisplayName("Caso de testes em que é inserido um valor de hora maior que 40h")
+    public void testarConstrutorEntradaValorHoraInvalidaLimiteSuperior() {
+        // Arrange
+        Integer entradaInvalida = 41;
+        String mensagemEsperada = "As horas trabalhadas devem ser entre 20 e 40h";
+
+        // act
+        Throwable e = assertThrows(IllegalArgumentException.class,
+                () -> {
+                    funcionario = new Funcionario(entradaInvalida);
+                });
+        assertEquals(mensagemEsperada, e.getMessage());
+    }
+
+    @Test
+    @DisplayName("Caso de testes em que é inserido um valor de hora menor que 20h")
+    public void testarConstrutorEntradaValorHoraInvalidaLimiteInferior() {
+        // Arrange
+        Integer entradaInvalida = 19;
+        String mensagemEsperada = "As horas trabalhadas devem ser entre 20 e 40h";
+
+        // act
+        Throwable e = assertThrows(IllegalArgumentException.class,
+                () -> {
+                    funcionario = new Funcionario(entradaInvalida);
+                });
+        assertEquals(mensagemEsperada, e.getMessage());
     }
 }
